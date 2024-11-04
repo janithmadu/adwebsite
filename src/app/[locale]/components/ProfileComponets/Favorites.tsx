935c4e050d8594fe873e9ca2e9f37a708d6863cf
Janith Madushan <Janithm@orelit.com>
2024-11-04 22:54:25 +0530

    Ad  Section Development:2024-10-05 Note:Complete Sanity intergrate

--- src/app/[locale]/components/ProfileComponets/Favorites.tsx
+++ src/app/[locale]/components/ProfileComponets/Favorites.tsx
@@ -42,7 +42,7 @@ const Favorites: React.FC<MainProfileProps> = ({ UserAds }) => {
               height={160}
               />
             <h2 className="text-2xl font-semibold text-gray-700">
-              Oops! No Favorites Ads.
+              Oops! No Favorites Adss.
             </h2>
           </div>
         </div>
