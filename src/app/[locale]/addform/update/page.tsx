2953138dafcaf791254cd66af68499fa25e2fdc0
janithmadu <janithmadu281@gmail.com>
2024-11-11 14:28:23 +0530

    Ad  Section Development:2024-10-05 Note:Fix issue of home page ads section

--- src/app/[locale]/addform/update/page.tsx
+++ src/app/[locale]/addform/update/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import UpdateForm from '../../components/Forms/UpdateForm';
+
 import { getAllCategory } from '../../actions/getCategories';
 
 const Page = async () => {
     const GetCategory = await getAllCategory();
     return (
         <div>
-            <UpdateForm categories={GetCategory}/>
+            Update
         </div>
     );
 }
